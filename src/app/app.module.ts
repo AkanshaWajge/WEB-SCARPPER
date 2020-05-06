2904ba0eca211c99f00950af5b95b57f558e0281
nikita.kumari <nikita.kumari@synerzip.com>
2020-05-06 20:13:58 +0530

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { ExtractorsComponent } from './extractors/extractors.component';
 import { SupportComponent } from './support/support.component';
 import { EditComponent } from './extractors/edit/edit.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { EditComponent } from './extractors/edit/edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
