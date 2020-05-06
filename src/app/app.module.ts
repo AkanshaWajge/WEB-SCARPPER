f9253e3acebf3e266cdc22ffd99faac456028d51
nikita.kumari <nikita.kumari@synerzip.com>
2020-05-06 19:13:48 +0530

    Edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ExtractorsComponent } from './extractors/extractors.component';
 import { SupportComponent } from './support/support.component';
+import { EditComponent } from './extractors/edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ExtractorsComponent,
-    SupportComponent
+    SupportComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
